cdcf7ea190ee5b90a362a48af3ab0f12567b9c61
ayobami olaniyan <olaniyanayobamijr@gmail.com>
2026-03-03 11:32:25 +0100

    feat: added Identity server - OAuth 2 and JWT

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Duende.IdentityModel;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                   new Client
+                   {
+                        ClientId = "inventoryClient",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+                        AllowedScopes = { "inventoryAPI" }
+                   },
+                   new Client
+                   {
+                       ClientId = "inventory_mvc_client",
+                       ClientName = "Inventory MVC Web App",
+                       AllowedGrantTypes = GrantTypes.Hybrid,
+                       RequirePkce = false,
+                       AllowRememberConsent = false,
+                       RedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signin-oidc"
+                       },
+                       PostLogoutRedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signout-callback-oidc"
+                       },
+                       ClientSecrets = new List<Secret>
+                       {
+                           new Secret("secret".Sha256())
+                       },
+                       AllowedScopes = new List<string>
+                       {
+                           IdentityServerConstants.StandardScopes.OpenId,
+                           IdentityServerConstants.StandardScopes.Profile,
+                           IdentityServerConstants.StandardScopes.Address,
+                           IdentityServerConstants.StandardScopes.Email,                           
+                           "inventoryAPI",
+                           "roles"
+                       }
+                   }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+           new ApiScope[]
+           {
+               new ApiScope("inventoryAPI", "Inventory API")
+           };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+               //new ApiResource("inventoryAPI", "Inventory API")
+          };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+          new IdentityResource[]
+          {
+              new IdentityResources.OpenId(),
+              new IdentityResources.Profile(),
+              new IdentityResources.Address(),
+              new IdentityResources.Email(),
+              new IdentityResource(
+                    "roles",
+                    "Your role(s)",
+                    new List<string>() { "role" })
+          };
+
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "mehmet",
+                    Password = "swn",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "mehmet"),
+                        new Claim(JwtClaimTypes.FamilyName, "ozkaya")
+                    }
+                }
+            };
+    }
+}
